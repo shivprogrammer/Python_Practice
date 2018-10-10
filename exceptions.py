3162750ef17cab6eb1b8d57419d4c50bfc218ecb
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 02:15:37 -0700

    using broad catch all exceptions

--- exceptions.py
+++ exceptions.py
@@ -4,9 +4,17 @@ student = {
     "feedback": None
 }
 
+student["last_name"] = "Kowalski"
+
 try:
     last_name = student["last_name"]
+    numbered_last_name = 3 + last_name
 except KeyError:
     print("Error finding last_name")
+except TypeError:
+    print("I can't add these two together!")
+except Exception:
+    print("Unknown error")
+
 
 print("This code executes!")
