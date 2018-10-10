fb5c5d4b5195a743f04d459c4e5e4a9ac9b8c35d
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 14:49:57 -0700

    handling variable number of arguments with key descriptions for corresponding values

--- functions.py
+++ functions.py
@@ -18,13 +18,13 @@ def add_student(name, student_id=332):
     students.append(student)
 
 
-def var_args(name, *args):
+def var_args(name, **kwargs):
     print(name)
-    print(args)
+    print(kwargs["description"], kwargs["feedback"])
 
 
 student_list = get_students_titlecase()
 
 add_student(name="Mark", student_id=15)
 
-var_args("Mark", "Loves Python", None, "Hello", True)
+var_args("Mark", description="Loves Python", feedback=None, cool_person=True)
