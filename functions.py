32de542e5f6089c62409f0df105fecfaa09a7b88
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 15:53:32 -0700

    finally correctly iterated through dictionary in python

--- functions.py
+++ functions.py
@@ -1,43 +1,45 @@
-# students = []
+students = []
 
 
-# def get_students_titlecase():
-#     students_titlecase = []
-#     for student in students:
-#         students_titlecase = student["name"].title()
-#     return students_titlecase
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase.append(student["name"].title())
+    return students_titlecase
 
 
-# def print_students_titlecase():
-#     students_titlecase = get_students_titlecase()
-#     print(students_titlecase)
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
 
 
-# def add_student(name, student_id=332):
-#     student = {"name": name, "student_id": student_id}
-#     students.append(student)
+def add_student(name, student_id=332):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
 
 
-# user_wants_to_add_student = input("Would you like to add a student? (yes/no): ").lower()
+user_wants_to_add_student = input("Would you like to add a student? (yes/no): ").lower()
 
-# while user_wants_to_add_student == "yes":
-#     name = input("Enter student name: ")
-#     student_id = int(input("Enter student ID: "))
-#     add_student(name, student_id)
-#     print("")
-#     user_wants_to_add_student = input("Would you like to add another student? (yes/no): ").lower()
-
-
-def get_students():
-    students = ["mark", "james"]
-
-    def get_students_titlecase():
-        students_titlecase = []
-        for student in students:
-            students_titlecase.append(student.title())
-        return students_titlecase
-    students_titlecase_names = get_students_titlecase()
-    print(students_titlecase_names)
+while user_wants_to_add_student == "yes":
+    name = input("Enter student name: ")
+    student_id = int(input("Enter student ID: "))
+    add_student(name, student_id)
+    print("")
+    user_wants_to_add_student = input("Would you like to add another student? (yes/no): ").lower()
 
 
-get_students()
+print_students_titlecase()
+
+# def get_students():
+#     students = ["mark", "james"]
+#
+#     def get_students_titlecase():
+#         students_titlecase = []
+#         for student in students:
+#             students_titlecase.append(student.title())
+#         return students_titlecase
+#     students_titlecase_names = get_students_titlecase()
+#     print(students_titlecase_names)
+#
+#
+# get_students()
