9481522cab1b4f26872ab64f069692f380552ae8
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 14:28:18 -0700

    building upon functions file

--- functions.py
+++ functions.py
@@ -3,6 +3,16 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        student_titlecase = student.title()
+        students_titlecase = student.title()
     return students_titlecase
 
+def print_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        student_titlecase = student.title()
+    print(students_titlecase)
+
+def add_student(name):
+    students.append(name)
+
+student_list = get_students_titlecase()
