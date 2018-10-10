fb9e7fbf1955e54100b18fcf07a7c107dd7e655c
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 14:23:23 -0700

    python function practice

--- functions.py
+++ functions.py
@@ -1,2 +1,8 @@
 students = []
 
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        student_titlecase = student.title()
+    return students_titlecase
+
