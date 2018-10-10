f4919ad5eec11e89254673ed8d214335eff215c4
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 14:50:39 -0700

    functions file cleanup

--- functions.py
+++ functions.py
@@ -18,13 +18,4 @@ def add_student(name, student_id=332):
     students.append(student)
 
 
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
-
-
 student_list = get_students_titlecase()
-
-add_student(name="Mark", student_id=15)
-
-var_args("Mark", description="Loves Python", feedback=None, cool_person=True)
