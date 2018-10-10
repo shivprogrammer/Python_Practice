dff401aad47dd9a08705fa41d8e32228908709c4
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 02:26:26 -0700

    working with python functions

--- exceptions.py
+++ exceptions.py
@@ -11,8 +11,9 @@ try:
     numbered_last_name = 3 + last_name
 except KeyError:
     print("Error finding last_name")
-except TypeError:
+except TypeError as error:
     print("I can't add these two together!")
+    print(error)
 except Exception:
     print("Unknown error")
 
