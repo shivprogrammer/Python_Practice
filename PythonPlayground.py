f366f5a97ea8b6953a5028803ae4dd6d09c9c1b0
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 01:29:58 -0700

    more python for loop work

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -1,5 +1,13 @@
-x = 0
+# x = 0
 
-for index in range(10):
-    x += 10
-    print(f"The value of X is {x}")
+# for index in range(10):
+#     x += 10
+#     print(f"The value of X is {x}")
+
+student_names = ["James", "Katarina", "Jessica", "Mark", "Bort", "Frank Grimes", "Max Power"]
+
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found him! " + name)
+    print("Currently testing " + name)
