33beae829b36241061229ee66da168ace6aa0e5e
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 17:55:36 -0700

    working with python class constructors

--- classes.py
+++ classes.py
@@ -1,15 +1,10 @@
 students = []
 
+
 class Student:
-    def add_student(name, student_id=332):
+    def __init__(self, name, student_id=332):
         student = {"name": name, "student_id": student_id}
         students.append(student)
 
 
-student = Student()
-
-print(student)
-
-new_student = Student()
-
-print(new_student)
+print(students)
