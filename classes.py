f59ba00aebe0da99173cd004117bb38ded09ad39
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 19:37:42 -0700

    passing methods down from parent classes in python

--- classes.py
+++ classes.py
@@ -25,7 +25,12 @@ class HighSchoolStudent(Student):
     school_name = "Springfield High School"
 
     def get_school_name(self):
-        return self.school_name
+        return "This is a high school student"
+
+    def get_name_capitalize(self):
+        original_value = super().get_name_capitalize()
+        return original_value + "-HS"
+
 
 james = HighSchoolStudent("james")
 print(james.get_name_capitalize())
