82a5542c9d3e6484f8fe405c8ba342abef9e0744
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 17:30:58 -0700

    created read_students function to replace functionality of readlines

--- yield.py
+++ yield.py
@@ -4,11 +4,16 @@ students = []
 def read_file():
     try:
         f = open("students.txt", "r")
-        for student in f.readlines(): # for fucks sake please NEVER GOD DAMN CAPITALIZE LINES AGAIN GOD DAMN IT
+        for student in read_students(f):
             students.append(student)
         f.close()
     except Exception:
         print("Could not read file")
+
+
+def read_students(f):
+    for line in f:
+        yield line
 
 
 read_file()
