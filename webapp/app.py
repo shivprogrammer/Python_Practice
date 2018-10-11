4430b0aa61e7a13f80c8f2a44ae46ee40f0e399f
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 23:32:23 -0700

    building out app.py for the students page web application

---
+++ webapp/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from student import Student
+
+students = []
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def students_page():
+    if request.method == "POST":
+        new_student_id = request.form.get("student-id", "")
+        new_student_name = request.form.get("name", "")
+        
