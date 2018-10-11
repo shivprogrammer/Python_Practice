2f82c882b7c3ed6f1b0ac577b653fbb803fb6129
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 18:03:06 -0700

    added get_name_capitalize method and modified constructor of Student class

--- classes.py
+++ classes.py
@@ -3,11 +3,15 @@ students = []
 
 class Student:
     def __init__(self, name, student_id=332):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student = student_id
+        students.append(self)
 
     def __str__(self):
         return "Student"
+
+    def get_name_capitalize(selfs):
+        return self.name.capitalize()
 
 
 mark = Student("Mark")
