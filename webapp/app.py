5130c9ac0f7848bdc1dd47ecf1d5415e34da26d9
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-10 23:35:11 -0700

    building out app.py with the ability to post and retrive new student

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 from student import Student
 
@@ -11,4 +11,11 @@ def students_page():
     if request.method == "POST":
         new_student_id = request.form.get("student-id", "")
         new_student_name = request.form.get("name", "")
-        
+        new_student_last_name = request.form.get("last-name", "")
+
+        new_student = Student(name="new_student_name", student_id=new_student_id)
+        students.append(new_student)
+
+        return redirect(url_for("students_page"))
+
+    return render_template("index.html", student=students)
